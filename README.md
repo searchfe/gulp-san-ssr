980bc8b6abbf5a475f0a47f8e4273f15a7fd6b2f
harttle <yangjvn@126.com>
2019-10-25 19:04:46 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,8 @@
+# gulp-san-ssr
+![Language](https://img.shields.io/badge/-TypeScript-blue.svg)
+[![npm package](https://img.shields.io/npm/v/gulp-san-ssr.svg)](https://www.npmjs.org/package/gulp-san-ssr)
+[![npm downloads](http://img.shields.io/npm/dm/gulp-san-ssr.svg)](https://www.npmjs.org/package/gulp-san-ssr)
+
+gulp-san-ssr [san-ssr][san-ssr] 的 gulp 封装，除了新增的 target 等接口外，都直接透传给 san-ssr。
+
+[san-ssr]: https://github.com/searchfe/san-ssr
---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import { extname } from 'path';
+import * as PluginError from 'plugin-error';
+import { ToJSCompiler, ToPHPCompiler } from 'san-ssr';
+import through2 from 'through2';
+
+interface SSROptions {
+    [key: string]: string;
+}
+
+const PLUGIN_NAME = 'gulp-san-ssr';
+
+export function sanssr(options: SSROptions = {}) {
+    return through2.obj(function(file, _, cb) {
+        if (file.isNull()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'File: "' + file.relative + '" without content. You have to read it with gulp.src(..)'));
+            return;
+        }
+
+        if (file.isStream()) {
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            cb();
+            return;
+        }
+
+        if (file.isBuffer()) {
+            const targetCode = compile(file, options.target, options);
+            file.contents = Buffer.from(targetCode);
+            const path = file.path;
+            const ext = extname(path);
+            file.path = path.substr(0, path.length - ext.length) + '.' + options.target;
+        }
+
+        cb(null, file);
+    });
+}
+
+function compile(file, target, ssrOptions) {
+    const Compiler = target === 'php' ? ToPHPCompiler : ToJSCompiler;
+    const compiler = new Compiler(ssrOptions);
+    const targetCode = compiler.compile(file.path, ssrOptions);
+    return targetCode;
+}
